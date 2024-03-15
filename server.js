826283da4abf476b06718a67f68baadb7f5cd97f
Kykesh <150619538+Kykesh@users.noreply.github.com>
2024-03-14 20:58:39 -0500

    Refactor public assets, segregate CSS/JS for Handlebars views
    This commit organizes public assets into dedicated CSS and JS directories for better maintainability and performance optimization. We've moved inline styles and scripts from the Handlebars templates into separate CSS and JS files. This not only improves the readability of our Handlebars templates but also adheres to best practices by separating concerns.
    
    Each Handlebars view now includes its specific stylesheet and script file, enhancing the page load speed by avoiding the load of unnecessary assets on pages where they aren't needed. Additionally, the server has been configured to correctly serve static files from the public directory.
    
    Changes made:
    - Created css and js directories within the public folder.
    - Migrated inline CSS from Handlebars templates into respective .css files.
    - Migrated inline JavaScript from Handlebars templates into respective .js files.
    - Updated the Express static files middleware to correctly serve the new asset structure.
    - Removed the '/public' prefix from asset paths in the Handlebars templates to align with Express's static serving configuration.
    
    The changes contribute to a cleaner codebase and set the foundation for future development and potential features that require client-side scripting.

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 app.use('/', appointmentRoutes);
+app.use(express.static('public'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
